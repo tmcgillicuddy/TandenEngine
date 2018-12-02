1c105b04b87e5ec6bd28a75e0756de1d08c53cda
Thomas McGillicuddy <thomas.mcgillicuddy@mymail.champlain.edu>
2018-12-01 23:08:28 -0500

    Fixing string reading error just now need to convert the incoming string data to proper Component info

--- TandenEngine/Source/Engine/Entity/Components/Renderer.h
+++ TandenEngine/Source/Engine/Entity/Components/Renderer.h
@@ -19,7 +19,7 @@ namespace TandenEngine {
         void Register() override;
 
         std::string ToString() override {return std::string();};
-        Component * ConvertFromString(std::string input) override { return NULL;}; //Takes in some string data and will output a renderer object
+        Component * ConvertFromString(std::vector<std::string>) override { return NULL;}; //Takes in some string data and will output a renderer object
     };
 
 }
