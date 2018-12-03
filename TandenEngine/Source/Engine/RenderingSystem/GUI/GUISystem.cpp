270f3a974f435fa58c23ea3436da376871cc3491
Thomas McGillicuddy <thomas.mcgillicuddy@mymail.champlain.edu>
2018-12-03 15:32:40 -0500

    Adding gui element base class

--- TandenEngine/Source/Engine/RenderingSystem/GUI/GUISystem.cpp
+++ TandenEngine/Source/Engine/RenderingSystem/GUI/GUISystem.cpp
@@ -6,5 +6,16 @@
 #include "imgui.h"
 namespace TandenEngine
 {
+    std::vector<GUIElement *> GUISystem::mGuiElements;
 
+    void GUISystem::DrawGUI() {
+        for (const auto &element: mGuiElements)
+        {
+            element->DrawGUI();
+        }
+    }
+
+    void GUISystem::RegisterGUIElement(GUIElement *newElement) {
+        mGuiElements.emplace_back(newElement);
+    }
 }
