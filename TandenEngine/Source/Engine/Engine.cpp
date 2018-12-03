8a09644d0df0b499b3da8f660d1d89edb721ab12
Thomas McGillicuddy <thomas.mcgillicuddy@mymail.champlain.edu>
2018-12-02 21:02:44 -0500

    Added vulkan and glfw support

--- TandenEngine/Source/Engine/Engine.cpp
+++ TandenEngine/Source/Engine/Engine.cpp
@@ -39,22 +39,7 @@ namespace TandenEngine {
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
         GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
 
-        uint32_t extensionCount = 0;
-        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 
-        std::cout << extensionCount << " extensions supported" << std::endl;
-
-        glm::mat4 matrix;
-        glm::vec4 vec;
-        auto test = matrix * vec;
-
-        while(!glfwWindowShouldClose(window)) {
-            glfwPollEvents();
-        }
-
-        glfwDestroyWindow(window);
-
-        glfwTerminate();
 
 
 
@@ -68,6 +53,9 @@ namespace TandenEngine {
         ResourceManager::AddResource(newScene);
 
         system("pause");
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
     }
 
     void Engine::RunEngine() {
@@ -99,7 +87,6 @@ namespace TandenEngine {
 
     void Engine::StopEngine() {
         std::cout << "Closing Engine\n";
-        ResourceManager::SaveProjectResources();
     }
 
     Engine::Engine() {
