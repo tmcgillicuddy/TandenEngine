7e86d5afccc085d4b6f1168c03804f90bdc3f486
Thomas McGillicuddy <thomas.mcgillicuddy@mymail.champlain.edu>
2018-12-03 15:01:52 -0500

    Adding gui system base class to project

---
+++ TandenEngine/Source/Engine/RenderingSystem/GUI/GUISystem.h
@@ -0,0 +1,14 @@
+//
+// Created by thomas.mcgillicuddy on 12/3/2018.
+//
+
+#ifndef TANDENENGINE_GUISYSTEM_H
+#define TANDENENGINE_GUISYSTEM_H
+
+namespace TandenEngine {
+    class GUISystem {
+
+    };
+}
+
+#endif //TANDENENGINE_GUISYSTEM_H
