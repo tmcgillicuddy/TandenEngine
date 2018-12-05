628c5cbdbe39512ee5b1adbb18ee0c1c2fa9423f
Rosser Ricardo Martinez <rosser.martinez@gmail.com>
2018-12-04 21:52:21 -0500

    added physical and logical device initialization

--- TandenEngine/Source/Engine/RenderingSystem/RenderingSystem.cpp
+++ TandenEngine/Source/Engine/RenderingSystem/RenderingSystem.cpp
@@ -4,6 +4,9 @@
 
 #include "RenderingSystem.h"
 
+
+
+
 namespace TandenEngine {
 
 
@@ -12,6 +15,12 @@ namespace TandenEngine {
     std::vector<Renderer *> RenderingSystem::mRenderers;
 
     Window*  RenderingSystem::testWindow;
+
+    VkPhysicalDevice RenderingSystem::physicalDevice;
+    VkDevice RenderingSystem::logicalDevice;
+    VkQueue RenderingSystem::gfxQueue;
+    VkSurfaceKHR RenderingSystem::windowSurface;
+
 
     void RenderingSystem::Draw() {
         for (const auto &rend : mRenderers) {
@@ -46,6 +55,14 @@ namespace TandenEngine {
 
     void RenderingSystem::InitVulkan()
     {
+        InitVKInstance();
+        SelectPhysicalDevice();
+        InitLogicalDevice();
+    }
+
+
+    void RenderingSystem::InitVKInstance()
+    {
         //create instance
         VkApplicationInfo appInfo = {};
         appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
@@ -75,12 +92,117 @@ namespace TandenEngine {
             throw std::runtime_error("failed to create instance!");
         }
 
+    }
 
+    void RenderingSystem::SelectPhysicalDevice()
+    {
 
+        physicalDevice = VK_NULL_HANDLE;
 
-        //pick physical device
+        uint32_t deviceCount = 0;
+        vkEnumeratePhysicalDevices(VulkanInstance , &deviceCount, nullptr);
+
+        if (deviceCount == 0) {
+            throw std::runtime_error("failed to find GPUs with Vulkan support!");
+        }
+
+        std::vector<VkPhysicalDevice> devices(deviceCount);
+        vkEnumeratePhysicalDevices(VulkanInstance, &deviceCount, devices.data());
+
+        for (const auto& device : devices) {
+            if (SuitableDevice(device)) {
+                physicalDevice = device;
+                break;
+            }
+        }
+
+        if (physicalDevice == VK_NULL_HANDLE) {
+            throw std::runtime_error("failed to find a suitable GPU!");
+        }
 
     }
+
+    void RenderingSystem::InitLogicalDevice()
+    {
+
+        QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+
+        //look for queue family with capacity for graphics
+        VkDeviceQueueCreateInfo queueCreateInfo = {};
+        queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+        queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
+        queueCreateInfo.queueCount = 1;
+
+        //set priorities for queues, value between 0 and 1 to have priority when queueing events, necessary even if we are only using one queue for event execution
+        float queuePriority = 1.0f;
+        queueCreateInfo.pQueuePriorities = &queuePriority;
+
+
+        //check for special device features, like different shader types but right now, again, we dont give a shit
+        VkPhysicalDeviceFeatures deviceFeatures = {};
+
+        //create empty info for the new logical device
+        VkDeviceCreateInfo newDeviceInfo = {};
+        newDeviceInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+
+        //
+        newDeviceInfo.pQueueCreateInfos = &queueCreateInfo;
+        newDeviceInfo.queueCreateInfoCount = 1;
+
+        //give it the empty features object we created
+        newDeviceInfo.pEnabledFeatures = &deviceFeatures;
+
+        newDeviceInfo.enabledExtensionCount = 0;
+
+        //create the actual device and set it to our instance of logical device
+        if (vkCreateDevice(physicalDevice, &newDeviceInfo, nullptr, &logicalDevice) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create logical device!");
+        }
+
+        //get one queue from queue family of logical device and set it to the graphics queue
+        vkGetDeviceQueue(logicalDevice, indices.graphicsFamily.value(), 0, &gfxQueue);
+
+
+
+    }
+
+    QueueFamilyIndices RenderingSystem::findQueueFamilies(VkPhysicalDevice device)
+    {
+        QueueFamilyIndices indices;
+
+        //get queue families supported by target device
+        uint32_t queueFamilyCount = 0;
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+        //load found queue families into a vector
+        std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+        //iterate through queue family vec until we find a queue family on the graphcis card that can support VK_QUEUE_GRAPHICS_BIT
+        int i = 0;
+        for (const auto& queueFamily : queueFamilies) {
+            if (queueFamily.queueCount > 0 && queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+                indices.graphicsFamily = i;
+            }
+
+            if (indices.isComplete()) {
+                break;
+            }
+
+            ++i;
+        }
+
+        return indices;
+
+    }
+
+    bool RenderingSystem::SuitableDevice(VkPhysicalDevice targetDevice)
+    {
+        QueueFamilyIndices indices = findQueueFamilies(targetDevice);
+
+        return indices.isComplete();
+    }
+
 
 
 
@@ -108,7 +230,10 @@ namespace TandenEngine {
     void RenderingSystem::Cleanup()
     {
 
+        //Bring it on! I'll destroy you all!
         vkDestroyInstance(VulkanInstance, nullptr);
+
+        vkDestroyDevice(logicalDevice, nullptr);
 
         glfwDestroyWindow(testWindow->GetWindowRef());
 
--- TandenEngine/Source/Engine/RenderingSystem/RenderingSystem.h
+++ TandenEngine/Source/Engine/RenderingSystem/RenderingSystem.h
@@ -1,5 +1,6 @@
 //
 // Created by thomas.mcgillicuddy on 10/31/2018.
+// lots of help from vulkan-tutorial.com because I am still learning to set up a vulkan renderer in the first place -Rosser
 //
 
 #ifndef RENDERINGSYSTEM_H
@@ -10,30 +11,56 @@
 #include <vulkan/vulkan.h>
 #include "../Entity/Components/Renderer.h"
 #include "../../../Libraries/GLFW/include/GLFW/glfw3.h"
+#include <optional>
 
 namespace TandenEngine {
+
+    //struct to check if graphics card can actually handle Vulkan queues
+    struct QueueFamilyIndices {
+        std::optional<uint32_t> graphicsFamily;
+
+        bool isComplete() {return graphicsFamily.has_value();}
+    };
+
 
     class RenderingSystem {
     private:
 
-        static VkInstance VulkanInstance;
+        static VkInstance VulkanInstance;                   //vulkan instance
+        static VkPhysicalDevice physicalDevice;             //our graphics card
+        static VkDevice logicalDevice;                      //logical interface with graphics card
+        static VkQueue gfxQueue;                            //graphics queue for graphics events
+        static VkSurfaceKHR windowSurface;                  //surface of window
+        static std::vector<Renderer *> mRenderers;          //vector of renderers
+        static Window* testWindow;                          //window instance (just one for now)
 
-        static std::vector<Renderer *> mRenderers;
-
-        static Window* testWindow;
-
+        //init
         static void InitGLFW();
         static void InitVulkan();
-        static void InitWindow(int windowWidth, int windowHeight, std::string windowName);
-        static void PollWindowEvents();
+
+        //InitVulkan comprised of
+            static void InitVKInstance();                                                       //creating instance
+
+            static void SelectPhysicalDevice();                                                 //polling and selecting a graphics card to use
+            static bool SuitableDevice(VkPhysicalDevice targetDevice);                          //checking if a graphics card is suitable
+            static QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);               //checking queue families of graphics card so VK can send events through it
+
+            static void InitLogicalDevice();
+
+        static void InitWindow(int windowWidth, int windowHeight, std::string windowName);      // init window instance
+
+        static void PollWindowEvents();                                                         //maintain during engine runs
+
 
     public:
         static void Draw();
 
         static void RegisterRenderer(Renderer *newRenderer);
 
+        //init
         static void InitSystem();
 
+        //cleanup
         static void Cleanup();
 
     };
