9ced96175dd9aae15955015a8b9025f8dc04efdf
Nils Steinbuegl <nils1723@gmail.com>
2018-12-07 01:42:11 -0500

    Set up Collider checks for collider reference structure

--- TandenEngine/Source/Engine/PhysicsSystem/PhysicsSystem.cpp
+++ TandenEngine/Source/Engine/PhysicsSystem/PhysicsSystem.cpp
@@ -45,50 +45,60 @@ namespace TandenEngine {
         for(int i = 0; i < n; ++i) {
             for(int j = i + 1; j < n; ++j) {
 
+                colA = mColliders[i];
+                colB = mColliders[j];
 
-
-                /*
-                //AABB Boxes
-                if((mColliders[i]->mPosition.x - mSize.x/2) <= (mColliders[j]->mPosition.x + mSize.x/2) && (mColliders[i]->mPosition.x + mSize.x/2) >= (mColliders[j]->mPosition.x - mSize.x/2)) {
-                    if((mColliders[i]->mPosition.y - mSize.y/2) <= (mColliders[j]->mPosition.y + mSize.y/2) && (mColliders[i]->mPosition.y + mSize.y/2) >= (mColliders[j]->mPosition.y - mSize.y/2)) {
-                        if((mColliders[i]->mPosition.z - mSize.z/2) <= (mColliders[j]->mPosition.z + mSize.z/2) && (mColliders[i]->mPosition.z + mSize.z/2) >= (mColliders[j]->mPosition.z - mSize.z/2)) {
-                            # TRIGGERED
+                if(colA->mType == BOXCOLLIDER && colB->mType == BOXCOLLIDER) {
+                    /*
+                    if((mColliders[i]->mPosition.x - mSize.x/2) <= (mColliders[j]->mPosition.x + mSize.x/2) && (mColliders[i]->mPosition.x + mSize.x/2) >= (mColliders[j]->mPosition.x - mSize.x/2)) {
+                        if((mColliders[i]->mPosition.y - mSize.y/2) <= (mColliders[j]->mPosition.y + mSize.y/2) && (mColliders[i]->mPosition.y + mSize.y/2) >= (mColliders[j]->mPosition.y - mSize.y/2)) {
+                            if((mColliders[i]->mPosition.z - mSize.z/2) <= (mColliders[j]->mPosition.z + mSize.z/2) && (mColliders[i]->mPosition.z + mSize.z/2) >= (mColliders[j]->mPosition.z - mSize.z/2)) {
+                                # TRIGGERED
+                            }
                         }
                     }
+                     */
                 }
-                // Sphere v Sphere
-                //distance should end up being its own function in NilsMath
-                float distance = sqrt(pow(mColliders[i]->mPosition.x - mColliders[j]->mPosition.x, 2.0) * pow(mColliders[i]->mPosition.y - mColliders[j]->mPosition.y, 2.0) * pow(mColliders[i]->mPosition.z - mColliders[j]->mPosition.z, 2.0));
-                if (distance < (mColliders[i]->mRadius + mColliders[j]->mRadius)) {
-                    # TRIGGERED
+                else if((colA->mType == SPHERECOLLIDER && colB->mType == SPHERECOLLIDER)) {
+                    /*
+                    // Sphere v Sphere
+                    //distance should end up being its own function in NilsMath
+                    float distance = sqrt(pow(mColliders[i]->mPosition.x - mColliders[j]->mPosition.x, 2.0) * pow(mColliders[i]->mPosition.y - mColliders[j]->mPosition.y, 2.0) * pow(mColliders[i]->mPosition.z - mColliders[j]->mPosition.z, 2.0));
+                    if (distance < (mColliders[i]->mRadius + mColliders[j]->mRadius)) {
+                        # TRIGGERED
+                    }
+                    */
+                }
+                else if((colA->mType == BOXCOLLIDER && colB->mType == SPHERECOLLIDER)) {
+                    /*
+                    // if box is i
+                    Vector3 testPoint;
+                    testPoint.x = max(mColliders[i]->mPosition.x - mSize.x/2, min(mColliders[j]->mPosition.x, mColliders[i]->mPosition.x + mSize.x/2));
+                    testPoint.y = max(mColliders[i]->mPosition.y - mSize.y/2, min(mColliders[j]->mPosition.y, mColliders[i]->mPosition.y + mSize.y/2));
+                    testPoint.z = max(mColliders[i]->mPosition.z - mSize.z/2, min(mColliders[j]->mPosition.z, mColliders[i]->mPosition.z + mSize.z/2));
+
+                    float distance = sqrt(pow(testPoint.x - mColliders[j]->mPosition.x, 2.0) * pow(testPoint.y - mColliders[j]->mPosition.y, 2.0) * pow(testPoint.z - mColliders[j]->mPosition.z, 2.0));
+                    if(distance < mColliders[j]->radius) {
+                        # TRIGGERED
+                    }
+                    */
+                }
+                else if((colA->mType == SPHERECOLLIDER && colB->mType == BOXCOLLIDER)) {
+                    /*
+                    // if box is j
+                    Vector3 testPoint;
+                    testPoint.x = max(mColliders[j]->mPosition.x - mSize.x/2, min(mColliders[i]->mPosition.x, mColliders[j]->mPosition.x + mSize.x/2));
+                    testPoint.y = max(mColliders[j]->mPosition.y - mSize.y/2, min(mColliders[i]->mPosition.y, mColliders[j]->mPosition.y + mSize.y/2));
+                    testPoint.z = max(mColliders[j]->mPosition.z - mSize.z/2, min(mColliders[i]->mPosition.z, mColliders[j]->mPosition.z + mSize.z/2));
+
+                    float distance = sqrt(pow(testPoint.x - mColliders[i]->mPosition.x, 2.0) * pow(testPoint.y - mColliders[i]->mPosition.y, 2.0) * pow(testPoint.z - mColliders[i]->mPosition.z, 2.0));
+                    if(distance < mColliders[i]->radius) {
+                        # TRIGGERED
+                    }
+                    */
                 }
 
-                // Box v Sphere
-                // if box is i
-                Vector3 testPoint;
-                testPoint.x = max(mColliders[i]->mPosition.x - mSize.x/2, min(mColliders[j]->mPosition.x, mColliders[i]->mPosition.x + mSize.x/2));
-                testPoint.y = max(mColliders[i]->mPosition.y - mSize.y/2, min(mColliders[j]->mPosition.y, mColliders[i]->mPosition.y + mSize.y/2));
-                testPoint.z = max(mColliders[i]->mPosition.z - mSize.z/2, min(mColliders[j]->mPosition.z, mColliders[i]->mPosition.z + mSize.z/2));
-
-                float distance = sqrt(pow(testPoint.x - mColliders[j]->mPosition.x, 2.0) * pow(testPoint.y - mColliders[j]->mPosition.y, 2.0) * pow(testPoint.z - mColliders[j]->mPosition.z, 2.0));
-                if(distance < mColliders[j]->radius) {
-                    # TRIGGERED
-                }
-
-                // if box is j
-                Vector3 testPoint;
-                testPoint.x = max(mColliders[j]->mPosition.x - mSize.x/2, min(mColliders[i]->mPosition.x, mColliders[j]->mPosition.x + mSize.x/2));
-                testPoint.y = max(mColliders[j]->mPosition.y - mSize.y/2, min(mColliders[i]->mPosition.y, mColliders[j]->mPosition.y + mSize.y/2));
-                testPoint.z = max(mColliders[j]->mPosition.z - mSize.z/2, min(mColliders[i]->mPosition.z, mColliders[j]->mPosition.z + mSize.z/2));
-
-                float distance = sqrt(pow(testPoint.x - mColliders[i]->mPosition.x, 2.0) * pow(testPoint.y - mColliders[i]->mPosition.y, 2.0) * pow(testPoint.z - mColliders[i]->mPosition.z, 2.0));
-                if(distance < mColliders[i]->radius) {
-                    # TRIGGERED
-                }
-
-                # not triggered
-
-                */
+                std::cout << "No Collision between " << colA->mType << " and " << colB->mType << std::endl;
             }
         }
     }
