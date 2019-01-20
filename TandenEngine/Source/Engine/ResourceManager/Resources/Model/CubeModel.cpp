0ac1cf9499b23f29816f2c64bd547ee316935c15
DESKTOP-M64P5B1\Thomas <tmcgillicuddy12@gmail.com>
2019-01-19 23:50:42 -0500

    Replacing shape class with derived model classes for primitives

---
+++ TandenEngine/Source/Engine/ResourceManager/Resources/Model/CubeModel.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Thomas on 1/19/2019.
+//
+
+#include "CubeModel.h"
+
+namespace TandenEngine {
+    CubeModel::CubeModel() {
+
+    }
+}
--- TandenEngine/Source/Engine/ResourceManager/Resources/Model/CylinderModel.cpp
+++ TandenEngine/Source/Engine/ResourceManager/Resources/Model/CylinderModel.cpp
@@ -2,4 +2,4 @@
 // Created by Thomas on 1/19/2019.
 //
 
-#include "Shape.h"
+#include "CylinderModel.h"
