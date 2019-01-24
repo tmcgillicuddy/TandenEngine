a55316da52b0fef7ecda6a1acc453215d79e89b5
DESKTOP-M64P5B1\Thomas <tmcgillicuddy12@gmail.com>
2019-01-24 10:13:31 -0500

    Adding debugger directory

---
+++ TandenEngine/Source/Core/Debugger/Debug.h
@@ -0,0 +1,23 @@
+//
+// Created by Thomas on 1/24/2019.
+//
+
+#ifndef TANDENENGINE_DEBUG_H
+#define TANDENENGINE_DEBUG_H
+
+#include <string>
+#include <vector>
+
+namespace TandenEngine {
+
+    class Debug {
+        static std::vector<std::string> buffer;
+    public:
+        static void Log(std::string input); //Basic log API
+        static void LogError(std::string input);
+        static void DumpBuffer(); //Dump buffer to log file //TODO call from seperate thread
+    };
+
+}
+
+#endif //TANDENENGINE_DEBUG_H
