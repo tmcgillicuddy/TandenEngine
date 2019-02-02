2d86b03dc72fa72fb281c7684316ea1ea4a1e1c6
Thomas McGillicuddy <thomas.mcgillicuddy@mymail.champlain.edu>
2019-02-02 17:45:41 -0500

    using vulkan info class

--- TandenEngine/Source/Engine/ResourceManager/Resources/Image.cpp
+++ TandenEngine/Source/Engine/ResourceManager/Resources/Image.cpp
@@ -13,7 +13,7 @@ namespace TandenEngine {
         mResourceType = ResourceType::IMAGE;
     }
 
-    Image::Image(MetaData * inputMeta) {
+    Image::Image(MetaData *inputMeta) {
         mResourceType = ResourceType::IMAGE;
         mMetaData = inputMeta;
         CreateTextureImage();
@@ -25,7 +25,8 @@ namespace TandenEngine {
 
     void Image::CreateTextureImage() {
         int texWidth, texHeight, texChannels;
-        stbi_uc* pixels = stbi_load(mMetaData->mFileDir.c_str(), &texWidth, &texHeight, &texChannels, STBI_rgb_alpha); //Load image file data into stb pixels
+        stbi_uc *pixels = stbi_load(mMetaData->mFileDir.c_str(), &texWidth, &texHeight, &texChannels,
+                                    STBI_rgb_alpha); //Load image file data into stb pixels
         VkDeviceSize imageSize = texWidth * texHeight * 4;
 
         if (!pixels) {
@@ -36,24 +37,29 @@ namespace TandenEngine {
         VkDeviceMemory stagingBufferMemory;
 
         //Create a the buffer to hold the texture data
-        CreateBuffer(imageSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT  //TODO Require create buffer method
-        | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer, stagingBufferMemory);
+        CreateBuffer(imageSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+                     VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT  //TODO Require create buffer method
+                     | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer, stagingBufferMemory);
 
-        void* data;
-        vkMapMemory(device, stagingBufferMemory, 0, imageSize, 0, &data); //TODO Get device from rendering system
+        void *data;
+        vkMapMemory(RenderingSystem::GetVulkanInfo()->logicalDevice, stagingBufferMemory, 0, imageSize, 0, &data);
         memcpy(data, pixels, static_cast<size_t>(imageSize));
-        vkUnmapMemory(device, stagingBufferMemory); //TODO Get device from rendering system
+        vkUnmapMemory(RenderingSystem::GetVulkanInfo()->logicalDevice, stagingBufferMemory);
 
         stbi_image_free(pixels); //Free pixels
 
         VkImage textureImage;
         VkDeviceMemory textureImageMemory;
 
-        CreateImage(texWidth, texHeight, VK_FORMAT_R8G8B8A8_UNORM, VK_IMAGE_TILING_OPTIMAL, VK_IMAGE_USAGE_TRANSFER_DST_BIT
-        | VK_IMAGE_USAGE_SAMPLED_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, textureImage, textureImageMemory);
+        CreateImage(texWidth, texHeight, VK_FORMAT_R8G8B8A8_UNORM, VK_IMAGE_TILING_OPTIMAL,
+                    VK_IMAGE_USAGE_TRANSFER_DST_BIT
+                    | VK_IMAGE_USAGE_SAMPLED_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, textureImage,
+                    textureImageMemory);
     }
 
-    void Image::CreateImage(uint32_t width, uint32_t height, VkFormat format, VkImageTiling tiling, VkImageUsageFlags usage, VkMemoryPropertyFlags properties, VkImage& image, VkDeviceMemory& imageMemory) {
+    void
+    Image::CreateImage(uint32_t width, uint32_t height, VkFormat format, VkImageTiling tiling, VkImageUsageFlags usage,
+                       VkMemoryPropertyFlags properties, VkImage &image, VkDeviceMemory &imageMemory) {
         VkImageCreateInfo imageInfo = {};
         imageInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
         imageInfo.imageType = VK_IMAGE_TYPE_2D;
@@ -69,24 +75,30 @@ namespace TandenEngine {
         imageInfo.samples = VK_SAMPLE_COUNT_1_BIT;
         imageInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
 
-        if (vkCreateImage(device, &imageInfo, nullptr, &image) != VK_SUCCESS) { //TODO Get device from rendering system
+        if (vkCreateImage(RenderingSystem::GetVulkanInfo()->logicalDevice, &imageInfo, nullptr, &image) !=
+            VK_SUCCESS) { //TODO Get device from rendering system
             throw std::runtime_error("failed to create image!");
         }
 
         //Get memory requirements for image
         VkMemoryRequirements memRequirements;
-        vkGetImageMemoryRequirements(device, image, &memRequirements);//TODO Get device from rendering system
+        vkGetImageMemoryRequirements(RenderingSystem::GetVulkanInfo()->logicalDevice, image,
+                                     &memRequirements);//TODO Get device from rendering system
 
         //Get allocation info for image
         VkMemoryAllocateInfo allocInfo = {};
         allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
         allocInfo.allocationSize = memRequirements.size;
-        allocInfo.memoryTypeIndex = findMemoryType(memRequirements.memoryTypeBits, properties); //TODO Require find memory type
+        allocInfo.memoryTypeIndex = findMemoryType(memRequirements.memoryTypeBits,
+                                                   properties); //TODO Require find memory type
 
-        if (vkAllocateMemory(device, &allocInfo, nullptr, &imageMemory) != VK_SUCCESS) {//TODO Get device from rendering system
+        if (vkAllocateMemory(RenderingSystem::GetVulkanInfo()->logicalDevice, &allocInfo, nullptr, &imageMemory) !=
+            VK_SUCCESS) {//TODO Get device from rendering system
             throw std::runtime_error("failed to allocate image memory!");
         }
 
         //Bind image to memory device
-        vkBindImageMemory(device, image, imageMemory, 0); //TODO Get device from rendering system
+        vkBindImageMemory(RenderingSystem::GetVulkanInfo()->logicalDevice, image, imageMemory,
+                          0); //TODO Get device from rendering system
+    }
 }
