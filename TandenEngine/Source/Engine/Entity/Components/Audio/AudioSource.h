ed1f364e761f62e9a70c540f95c3b5ca1f0170e2
tmcgillicuddy <tmcgillicuddy12@gmail.com>
2019-02-04 22:22:31 -0500

    ng linting errors with audio comps

--- TandenEngine/Source/Engine/Entity/Components/Audio/AudioSource.h
+++ TandenEngine/Source/Engine/Entity/Components/Audio/AudioSource.h
@@ -10,16 +10,17 @@
 namespace TandenEngine {
 
     class AudioSource : public Component {
-    public:
+     public:
         AudioSource();
         ~AudioSource();
 
-        //Component Overrides
+        // Component Overrides
         void Update() override;
         std::unique_ptr<Component> Clone() override;
         std::string ToString() override;
-        Component * ConvertFromString(std::vector<std::string> input) override; //Takes in some string data and will output a mesh collider object
+        // Takes in some string data and will output a mesh collider object
+        Component * ConvertFromString(std::vector<std::string> input) override;
     };
-}
+}  // namespace TandenEngine
 
-#endif //TANDENENGINE_AUDIOSOURCE_H
+#endif  // TANDENENGINE_AUDIOSOURCE_H
