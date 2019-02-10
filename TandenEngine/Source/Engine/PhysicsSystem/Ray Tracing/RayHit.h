59d260c14d9bcb352c818c97839b1fddb0a1c7ad
Thomas McGillicuddy <thomas.mcgillicuddy@mymail.champlain.edu>
2019-02-10 14:35:01 -0500

    Adding data to ray hit class

--- TandenEngine/Source/Engine/PhysicsSystem/Ray Tracing/RayHit.h
+++ TandenEngine/Source/Engine/PhysicsSystem/Ray Tracing/RayHit.h
@@ -5,9 +5,15 @@
 #ifndef TANDENENGINE_RAYHIT_H
 #define TANDENENGINE_RAYHIT_H
 
+#include "NilsMath.h"
+
 namespace TandenEngine {
 
     class RayHit {
+     public:
+        vec2 mUV;  // the UV of the hit point
+        vec3 mNormal;  // the normal of the hit surface
+        vec3 mPoint;  // Where in ws did the hit intersect
     };
 
 }  // namespace TandenEngine
