ef7bc907024b8163d1d6ddb5d27992fbeecf87d7
Thomas McGillicuddy <thomas.mcgillicuddy@mymail.champlain.edu>
2019-02-10 14:30:40 -0500

    adding ray hit class and moving ray tracing physics classes to another directory

---
+++ TandenEngine/Source/Engine/PhysicsSystem/Ray Tracing/RayHit.h
@@ -0,0 +1,15 @@
+//
+// Created by thomas.mcgillicuddy on 2/10/2019.
+//
+
+#ifndef TANDENENGINE_RAYHIT_H
+#define TANDENENGINE_RAYHIT_H
+
+namespace TandenEngine {
+
+    class RayHit {
+    };
+
+}  // namespace TandenEngine
+
+#endif  // TANDENENGINE_RAYHIT_H
