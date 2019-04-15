e3165a3e84e5bc9b7ed99b27ad3a88ab26108152
Nils Steinbuegl <nils1723@gmail.com>
2019-04-15 13:33:37 -0400

    Previous algorithm was clearly wrong

--- TandenEngine/Source/Engine/Entity/Components/Physics/MeshCollider.h
+++ TandenEngine/Source/Engine/Entity/Components/Physics/MeshCollider.h
@@ -12,6 +12,7 @@
 namespace TandenEngine {
 
     struct Triangle {
+        // TODO(Nils) This is definitely not correct. Need to look into better.
         Triangle(vec3 p1, vec3 p2, vec3 p3) { mVert1 = p1; mVert2 = p2; mVert3 = p3; };
         ~Triangle() {};
         vec3 mVert1;
